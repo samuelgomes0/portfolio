d6fd16d9e6222e4129379bac192c112cb7b16806
Samuel Gomes <samuel_gomes26@hotmail.com>
2025-07-09 02:02:23 -0300

    refactor: ajustar texto e remover parâmetros não utilizados no componente DotPattern

--- src/app/(sections)/About/index.tsx
+++ src/app/(sections)/About/index.tsx
@@ -6,7 +6,7 @@ function AboutSection() {
       <TypographyH3 text="Sobre mim" className="font-bold" />
       <div className="space-y-2">
         <TypographyMuted text="Atualmente atuo na Compass UOL, desenvolvendo soluções de IA conversacional e comunicação digital para o setor de saúde, com foco em fluxos de atendimento inteligentes, contact centers escaláveis e integrações robustas usando Twilio e Google Cloud Platform." />
-        <TypographyMuted text="Também lidero o Front-End em um projeto acadêmico que cria uma plataforma de tradução e revisão automatizada de textos científicos, aplicando IA para garantir qualidade técnica e eficiência na produção de artigos." />
+        <TypographyMuted text="Também lidero o Front-End em um projeto que cria uma plataforma de tradução e revisão automatizada de textos científicos, aplicando IA para garantir qualidade técnica e eficiência na produção de artigos." />
         <TypographyMuted text="Paralelamente, sou idealizador do Numora, uma plataforma SaaS de gestão financeira pessoal, criada para ajudar pessoas a entenderem e planejarem melhor sua vida financeira." />
       </div>
     </section>
